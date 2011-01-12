98ac131650ff25524232fdd253ced17b8449591b
Matthew Tidd <matt@dmtprogramming.com>
2011-01-12 16:27:03 -0500

    fixed the cursor to use the correct pointer and have the correct colored outline

--- src/screen.cpp
+++ src/screen.cpp
@@ -16,6 +16,8 @@ Screen::Screen(char * dsp)
 	} else {
 		_screen = xcb_setup_roots_iterator(xcb_get_setup(_conn)).data;
 		_visual = draw_screen_default_visual(_screen);
+		changeCursor();
+		setupBackground();
 	}
 
 	if (_instance == 0) {
@@ -59,4 +61,46 @@ xcb_visualtype_t* Screen::visual()
 bool Screen::connectionError()
 {
 	return(_connection_error);
+}
+
+void Screen::changeCursor()
+{
+	int cursor_index = XC_left_ptr;
+	// change the default cursor
+	xcb_font_t font = xcb_generate_id(_conn);
+	xcb_void_cookie_t fontCookie = xcb_open_font_checked(_conn,
+						font,
+						6,
+						"cursor" );
+
+	xcb_cursor_t cursor = xcb_generate_id(_conn);
+	xcb_create_glyph_cursor(_conn,
+						 cursor,
+						 font,
+						 font,
+						 cursor_index,
+						 cursor_index + 1,
+						 0, 0, 0, 0xFFFF, 0xFFFF, 0xFFFF );
+
+	xcb_gcontext_t gc = xcb_generate_id(_conn);
+
+	uint32_t mask = XCB_GC_FOREGROUND | XCB_GC_BACKGROUND | XCB_GC_FONT;
+	uint32_t values[3];
+	values[0] = _screen->black_pixel;
+	values[1] = _screen->white_pixel;
+	values[2] = font;
+
+	xcb_void_cookie_t gcCookie = xcb_create_gc_checked(_conn, gc, _screen->root, mask, values);
+
+	uint32_t value_list = cursor;
+	xcb_change_window_attributes(_conn, _screen->root, XCB_CW_CURSOR, &value_list);
+
+	xcb_free_cursor(_conn, cursor);
+
+	fontCookie = xcb_close_font_checked(_conn, font);
+}
+
+void Screen::setupBackground()
+{
+
 }
